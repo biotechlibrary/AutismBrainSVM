522716fc0f135635b70642a80a4772bd5d67282d
Pau Rotger Gonzalez <101683451+biotechlibrary@users.noreply.github.com>
2023-04-19 16:39:38 -0700

    Update data_sorter.py

--- SVM/data_sorter.py
+++ SVM/data_sorter.py
@@ -1,61 +1,43 @@
-'''
-This script utilizes the CSV file containing metadata for each patient. The script organizes the data into seperate folders 
-for autistic and control groups. 
-
-Replace 'path/to/your/metadata.csv' with the path to your metadata CSV file. This script will create two folders data/autistic and data/control, and move the respective .1D and .nii.gz files into the appropriate group folders.
-
-Before running this script, make sure to create a backup of your data as the script moves the files from the original folders to the new group folders.
-'''
 import os
 import shutil
 import pandas as pd
 
-# Load the metadata CSV file
-metadata_file = 'path/to/your/metadata.csv' #INSERT ACTUAL FILE PATH
+# Load the CSV metadata
+metadata_file = 'Phenotypic_V1_0b_preprocessed1.csv'
 metadata = pd.read_csv(metadata_file)
 
-# Set the paths to the original data and the destination folders
-data_path = 'Outputs'
-dest_path = 'data'
+# Define source directories
+source_dirs = [
+    '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/Outputs/ccs',
+    '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/Outputs/cpac/func_mean',
+    '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/Outputs/cpac/func_preproc',
+]
 
-autistic_path = os.path.join(dest_path, 'autistic')
-control_path = os.path.join(dest_path, 'control')
-
-# Create the destination folders if they don't exist
-os.makedirs(autistic_path, exist_ok=True)
-os.makedirs(control_path, exist_ok=True)
-
-# Define a mapping from pipeline names to their respective data subdirectories
-pipelines = {
-    'ccs': 'rois_ho',
-    'cpac': 'func_mean',
-    'cpac': 'func_preproc'
+# Define destination directories
+dest_dirs = {
+    'autistic': '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/parsed_data/autistic',
+    'control': '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/parsed_data/control',
 }
 
-# Iterate through the metadata, moving files to the appropriate group folders
-for _, row in metadata.iterrows():
-    patient_id = row['SUB_ID']
-    diagnosis_group = row['DX_GROUP']
-
-    # Determine the destination folder based on the diagnosis group
-    if diagnosis_group == 1:  # autistic
-        group_folder = autistic_path
-    else:  # control
-        group_folder = control_path
-
-    # Move files from each pipeline to the corresponding group folder
-    for pipeline, subdirectory in pipelines.items():
-        source_file = os.path.join(
-            data_path, pipeline, 'filt_noglobal', subdirectory, f'source_{patient_id}_{pipeline}'
-        )
+# Process files and move them to appropriate directories
+for src_dir in source_dirs:
+    for file_name in os.listdir(src_dir):
+        file_parts = file_name.split('_')
         
-        if subdirectory == 'rois_ho':
-            source_file += '.1D'
-        else:
-            source_file += '.nii.gz'
+        # Assuming the subject ID is the second part of the file name
+        subject_id = file_parts[1]
         
-        if os.path.exists(source_file):
-            dest_file = os.path.join(group_folder, os.path.basename(source_file))
-            shutil.move(source_file, dest_file)
-        else:
-            print(f"File not found: {source_file}")
+        try:
+            # Find the matching row in the metadata
+            matching_row = metadata.loc[metadata['subject'] == int(subject_id)]
+            
+            if not matching_row.empty:
+                # Determine the group (autistic or control) based on the 'DX_GROUP' column
+                group = 'autistic' if matching_row['DX_GROUP'].values[0] == 1 else 'control'
+                
+                # Move the file to the appropriate destination directory
+                src_file_path = os.path.join(src_dir, file_name)
+                dest_file_path = os.path.join(dest_dirs[group], file_name)
+                shutil.move(src_file_path, dest_file_path)
+        except ValueError:
+            print(f"Skipping file with non-integer subject ID: {file_name}")
