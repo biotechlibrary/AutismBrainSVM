0f9c68e3d6061532d2d986e9328512051abb299e
Pau Rotger Gonzalez <101683451+biotechlibrary@users.noreply.github.com>
2023-05-01 15:40:28 -0700

    Update brain_classifier.py

--- SVM/brain_classifier.py
+++ SVM/brain_classifier.py
@@ -1,30 +1,122 @@
 import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from skopt import BayesSearchCV
+from skopt.space import Real, Integer, Categorical
 
-# Load the extracted features and corresponding labels
-features = extract_features(correlation_matrices)
-labels = np.array(your_labels)  # Replace with your actual labels
+def load_data():
+    """
+    Load the data for the classification task.
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+    Returns:
+        tuple: A tuple containing the feature matrix (X) and the corresponding labels (y).
+    """
+    # Load your data here and return features (X) and labels (y)
+    pass
 
-# Define the SVM classifier and hyperparameter search space
-svm = SVC()
-param_grid = {'C': [0.1, 1, 10, 100], 'kernel': ['linear', 'rbf'], 'gamma': [0.1, 1, 10, 100]}
+def bayesian_optimization(X, y):
+    """
+    Perform Bayesian optimization to find the best hyperparameters for an SVM classifier.
 
-# Perform grid search with cross-validation
-grid_search = GridSearchCV(svm, param_grid, cv=5, scoring='accuracy')
-grid_search.fit(X_train, y_train)
+    Args:
+        X (numpy.ndarray): The feature matrix.
+        y (numpy.ndarray): The labels.
 
-# Train the classifier with the optimal hyperparameters
-best_params = grid_search.best_params_
-best_svm = SVC(**best_params)
-best_svm.fit(X_train, y_train)
+    Returns:
+        skopt.searchcv.BayesSearchCV: The BayesSearchCV object containing the search results.
+    """
+    # Create a SVM classifier
+    svm_clf = SVC()
 
-# Evaluate the classifier on the testing set
-y_pred = best_svm.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
-print(classification_report(y_test, y_pred))
+    # Specify hyperparameters and their ranges
+    param_space = {
+        "C": Real(1e-3, 1e3, prior="log-uniform"),
+        "kernel": Categorical(["linear", "rbf"]),
+        "gamma": Categorical(["scale", "auto"]) + Real(1e-3, 1e3, prior="log-uniform"),
+    }
+
+    # Perform Bayesian optimization
+    bayes_search = BayesSearchCV(
+        svm_clf, search_spaces=param_space, n_iter=100, cv=5, n_jobs=-1, verbose=2
+    )
+    bayes_search.fit(X, y)
+
+    return bayes_search
+
+def randomized_search(X, y):
+    """
+    Perform randomized search to find the best hyperparameters for an SVM classifier.
+
+    Args:
+        X (numpy.ndarray): The feature matrix.
+        y (numpy.ndarray): The labels.
+
+    Returns:
+        sklearn.model_selection.RandomizedSearchCV: The RandomizedSearchCV object containing the search results.
+    """
+    # Create a SVM classifier
+    svm_clf = SVC()
+
+    # Specify hyperparameters and their ranges
+    param_dist = {
+        "C": np.logspace(-3, 3, 7),
+        "kernel": ["linear", "rbf"],
+        "gamma": ["scale", "auto"] + list(np.logspace(-3, 3, 7)),
+    }
+
+    # Perform randomized search
+    random_search = RandomizedSearchCV(
+        svm_clf, param_distributions=param_dist, n_iter=100, cv=5, n_jobs=-1, verbose=2
+    )
+    random_search.fit(X, y)
+
+    return random_search
+
+def grid_search(X, y):
+    """
+    Perform grid search with cross-validation to find the best hyperparameters for an SVM classifier.
+
+    Args:
+        X (numpy.ndarray): The feature matrix.
+        y (numpy.ndarray): The labels.
+
+    Returns:
+        sklearn.model_selection.GridSearchCV: The GridSearchCV object containing the search results.
+    """
+    # Define the SVM classifier and hyperparameter search space
+    svm = SVC()
+    param_grid = {'C': [0.1, 1, 10, 100], 'kernel': ['linear', 'rbf'], 'gamma': [0.1, 1, 10, 100]}
+
+    # Perform grid search with cross-validation
+    grid_search = GridSearchCV(svm, param_grid, cv=5, scoring='accuracy
+                                   grid_search.fit(X, y)
+
+    return grid_search
+
+def main():
+    """
+    Main function to load data, perform hyperparameter optimization, train the best model, and evaluate its performance.
+    """
+    X, y = load_data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Choose your optimization method by uncommenting the corresponding line below
+    # search = bayesian_optimization(X_train, y_train)
+    # search = randomized_search(X_train, y_train)
+    search = grid_search(X_train, y_train)
+
+    best_params = search.best_params_
+    best_svm = SVC(**best_params)
+    best_svm.fit(X_train, y_train)
+
+    y_pred = best_svm.predict(X_test)
+    print("Classification report:")
+    print(classification_report(y_test, y_pred))
+    print("Accuracy score:", accuracy_score(y_test, y_pred))
+
+if __name__ == "__main__":
+    main()
+
