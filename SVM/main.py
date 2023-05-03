17a044f88b788ce64443a2dce2adc26d3a90a63d
Pau Rotger Gonzalez <101683451+biotechlibrary@users.noreply.github.com>
2023-05-03 14:27:37 -0700

    Update main.py

--- SVM/main.py
+++ SVM/main.py
@@ -1,30 +1,34 @@
-from analyze_preprocessed_fmri import analyze_preprocessed_fmri
 import os
 import glob
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor
+from analyze_preprocessed_fmri import analyze_preprocessed_fmri
+from feature_extraction import extract_features
+from brain_classifier import train_and_evaluate_classifier
+from nilearn.input_data import NiftiLabelsMasker
 
-# Set the input and output directories
-control_dir = '/path/to/control/fmri_data'
-autistic_dir = '/path/to/autistic/fmri_data'
-output_dir = '/path/to/output_dir'
 
-# Get the list of input files
+
+control_dir = '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/parsed_data/control'
+autistic_dir = '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/parsed_data/autistic'
+atlas_path = "/home/pau/Documents/Research/thesis/RSN_atlas_3mm.nii.gz"
+
 control_files = sorted(glob.glob(os.path.join(control_dir, "*.nii.gz")))
 autistic_files = sorted(glob.glob(os.path.join(autistic_dir, "*.nii.gz")))
 
-# Define a function to process a single file and return its label and correlation matrix
-def process_file(file_path, group_label, output_dir):
-    fisher_z_matrix = analyze_preprocessed_fmri(file_path, output_dir)
+def process_file(args):
+    file_path, group_label = args
+    fisher_z_matrix = analyze_preprocessed_fmri(file_path, atlas_path)
     return group_label, fisher_z_matrix
 
-# Process all files in parallel using concurrent.futures
+
+
 correlation_matrices = []
 labels = []
 
-with ProcessPoolExecutor() as executor:
-    control_results = executor.map(process_file, control_files, ['control'] * len(control_files), [output_dir] * len(control_files))
-    autistic_results = executor.map(process_file, autistic_files, ['autistic'] * len(autistic_files), [output_dir] * len(autistic_files))
+with ProcessPoolExecutor(max_workers=5) as executor:
+    control_results = executor.map(process_file, zip(control_files, ['control'] * len(control_files)))
+    autistic_results = executor.map(process_file, zip(autistic_files, ['autistic'] * len(autistic_files)))
 
     for group_label, fisher_z_matrix in control_results:
         labels.append(group_label)
@@ -34,6 +38,8 @@ with ProcessPoolExecutor() as executor:
         labels.append(group_label)
         correlation_matrices.append(fisher_z_matrix)
 
-# Save the correlation matrices and labels to disk
-np.save(os.path.join(output_dir, 'correlation_matrices.npy'), correlation_matrices)
-np.save(os.path.join(output_dir, 'labels.npy'), labels)
+# Feature extraction
+features = extract_features(correlation_matrices)
+
+# Classifier training and evaluation
+train_and_evaluate_classifier(features, labels)
