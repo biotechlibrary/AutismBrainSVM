43c85ef5c46491aeb71f3d6e2e0ee6c763a1d50a
Pau Rotger Gonzalez <101683451+biotechlibrary@users.noreply.github.com>
2023-05-13 14:27:08 -0700

    Update main.py

--- SVM/main.py
+++ SVM/main.py
@@ -7,7 +7,25 @@ from feature_extraction import extract_features
 from brain_classifier import train_and_evaluate_classifier
 from nilearn.input_data import NiftiLabelsMasker
 
+"""
+This script is used to process functional MRI (fMRI) data, perform feature extraction and train a classifier.
 
+First, it defines paths to directories containing preprocessed fMRI data for control and autistic groups,
+and an atlas used for region of interest (ROI) extraction. It also defines the function 'process_file' which
+performs analysis on a given fMRI file.
+
+It then uses a ProcessPoolExecutor to concurrently process fMRI files. The files are processed by computing
+the Fisher-Z transformation of the correlation matrix for each file. The resulting correlation matrices and 
+associated labels (control or autistic) are stored in lists.
+
+Next, the script performs feature extraction on the correlation matrices using Principal Component Analysis (PCA).
+
+Finally, it trains and evaluates a classifier on the extracted features and their associated labels.
+
+NOTE: This script relies on the functions 'analyze_preprocessed_fmri', 'extract_features' and 
+'train_and_evaluate_classifier' being defined in separate modules. Please ensure these modules are correctly 
+imported at the start of the script.
+"""
 
 control_dir = '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/parsed_data/control'
 autistic_dir = '/home/pau/micromamba/envs/extraction/AutismBrainSVM/SVM/parsed_data/autistic'
