966ac6210c9a7e5678b66208cc6caf51835079f8
Pau Rotger Gonzalez <101683451+biotechlibrary@users.noreply.github.com>
2023-05-13 14:23:20 -0700

    Update analyze_preprocessed_fmri.py

--- SVM/analyze_preprocessed_fmri.py
+++ SVM/analyze_preprocessed_fmri.py
@@ -3,8 +3,26 @@ import numpy as np
 from nilearn.connectome import ConnectivityMeasure
 from nilearn.input_data import NiftiLabelsMasker
 
-
 def analyze_preprocessed_fmri(input_file, atlas):
+    """
+    This function computes the functional connectivity matrix of the preprocessed fMRI data. 
+    It first extracts the time series from the ROIs defined by the atlas, then calculates the 
+    functional connectivity matrix, applies Fisher z-transformation to normalize the data, and 
+    replaces any NaN values with the mean of the matrix.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to the preprocessed fMRI data in NIfTI format.
+    atlas : str
+        Path to the brain atlas in NIfTI format used for defining ROIs.
+
+    Returns
+    -------
+    fisher_z_matrix : np.array
+        A 2D numpy array representing the Fisher z-transformed functional connectivity matrix.
+
+    """
     masker = NiftiLabelsMasker(labels_img=atlas, standardize=False)
     roi_time_series = masker.fit_transform(input_file)
 
@@ -15,6 +33,5 @@ def analyze_preprocessed_fmri(input_file, atlas):
 
     nan_mean = np.nanmean(fisher_z_matrix)
     fisher_z_matrix = np.where(np.isnan(fisher_z_matrix), nan_mean, fisher_z_matrix)
-
 
     return fisher_z_matrix
