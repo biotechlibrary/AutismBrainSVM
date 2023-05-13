3c158f7d1a33b34c51e80d4c2dbbce1417884b9b
Pau Rotger Gonzalez <101683451+biotechlibrary@users.noreply.github.com>
2023-05-13 14:26:02 -0700

    Update feature_extraction.py

--- SVM/feature_extraction.py
+++ SVM/feature_extraction.py
@@ -1,9 +1,24 @@
-import numpy as np
-from sklearn.decomposition import PCA
-
 def extract_features(correlation_matrices, n_components=None):
+    """
+    This function uses Principal Component Analysis (PCA) to extract features from a stack of correlation matrices.
+    If the number of components is not specified, it will be set to one less than the minimum dimension of the stacked matrices.
+
+    Parameters
+    ----------
+    correlation_matrices : list of np.array
+        A list of 2D numpy arrays, where each array represents a correlation matrix.
+
+    n_components : int, optional
+        The number of principal components to keep. If not specified, it will be set to one less than the minimum dimension
+        of the stacked matrices.
+
+    Returns
+    -------
+    pca_features : np.array
+        The features extracted using PCA. Each row corresponds to the features extracted from one correlation matrix.
+    """
     stacked_matrices = np.vstack(correlation_matrices)
-    
+
     if n_components is None:
         n_components = min(stacked_matrices.shape) - 1
 
